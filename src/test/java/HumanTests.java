8ec12360983661f5b9135a86d192e41c97740a5c
L00094131 <L00094131@student.lyit.ie>
2017-03-16 19:02:09 +0000

    Added two tests for setting weight too large and negative number.

--- src/test/java/HumanTests.java
+++ src/test/java/HumanTests.java
@@ -3,39 +3,52 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class HumanTests {
-	private Human testDummy = new Human("Joe", "Bloggs", "Deerpark", 140, 160, 23);
+	private Human myHuman = new Human("Joe", "Bloggs", "Deerpark", 140, 160, 23);
 
 	@Test
 	public void testGetFirstName() {
-		assertEquals("Joe", testDummy.getFName());
+		assertEquals("Joe", myHuman.getFName());
 	}
 
 	@Test
 	public void testGetSecondName() {
-		assertEquals("Bloggs", testDummy.getLName());
+		assertEquals("Bloggs", myHuman.getLName());
 	}
 	
 	
 	@Test
 	public void testGetAge() {
-		assertEquals(23, testDummy.getAge());
+		assertEquals(23, myHuman.getAge());
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testSetAgeNegativeThrowException(){
-		testDummy.setAge(-1);
-		assertEquals(-1, testDummy.getAge());
+	public void testSetAgeNegativeAgeException(){
+		myHuman.setAge(-1);
+		assertEquals(-1, myHuman.getAge());
 	}
 		
 	
 	@Test
-	public void testGetWeight() {
-		assertEquals(140, testDummy.getWeight());
+	public void testGetWeightInPounds() {
+		assertEquals(140, myHuman.getWeightInPounds());
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetWeightInPoundsTooHeavyException() {
+		myHuman.setWeightInPounds(351);
+		assertEquals(351, myHuman.getWeightInPounds());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetWeightInPoundsNegativeWeightException() {
+		myHuman.setWeightInPounds(-1);
+		assertEquals(-1, myHuman.getWeightInPounds());
+	}
+	
 	
 	@Test
 	public void testGetHeight() {
-		assertEquals(160, testDummy.getHeight());
+		assertEquals(160, myHuman.getHeight());
 	}
 	
 
