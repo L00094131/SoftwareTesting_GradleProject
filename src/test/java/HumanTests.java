eb6bbe598de78d5d7fdfbf39a7f11d5e76afe143
L00094131 <L00094131@student.lyit.ie>
2017-03-16 20:37:54 +0000

    Removed dummy test.

--- src/test/java/HumanTests.java
+++ src/test/java/HumanTests.java
@@ -10,10 +10,6 @@ public class HumanTests {
 		assertEquals("Joe", myHuman.getFName());
 	}
 	
-	@Test
-	public void testGetFirstName2() {
-		assertEquals("Joe", myHuman.getFName());
-	}
 
 	@Test
 	public void testGetSecondName() {
